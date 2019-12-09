d338e96069901335f792b446ee9f86c7ec15558c
Agnes Kadlot <agnes.kadlot@gmail.com>
2019-12-09 11:25:18 +0100

    Logger

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,14 +1,19 @@
 package org.leanpoker.player;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+        String list = request.getAsJsonObject().get("bet_index").getAsJsonArray().toString();
+        Logger LOGGER = Logger.getLogger(list);
         return 0;
     }
 
